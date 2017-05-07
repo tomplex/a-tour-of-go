115a4d6bfe6d8533f85e8cd9c6633b1604b03127
tom caruso <carusot42@gmail.com>
2017-05-07 12:56:11 -0400

    finishing maps exercises

--- exercise-maps.go
+++ exercise-maps.go
@@ -1,1 +1,25 @@
 package main
+
+/*
+Implement WordCount. It should return a map of the counts of each “word” in the string s.
+The wc.Test function runs a test suite against the provided function and prints success or failure.
+
+You might find strings.Fields helpful.
+ */
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func wordCount(s string) map[string]int {
+	count := make(map[string]int)
+	for _, w := range strings.Fields(s) {
+		count[w] += 1
+	}
+	return count
+}
+
+func main () {
+	wc.Test(wordCount)
+}
--- map-literals.go
+++ map-literals.go
@@ -9,6 +9,8 @@ type vertex struct {
 var m = map[string]vertex{
 	"Bell Labs" : vertex{40.68433, -74.39967},
 	"Google" : vertex{37.42202, -122.08408},
+	// when the value is a type, you can omit the declaration:
+	"White River Junction" : {43.649145, -72.321124},
 }
 
 func main() {
