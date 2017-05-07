848f7c709bbea71bdb652b33aa11f4b14c2be1ba
tom caruso <carusot42@gmail.com>
2017-05-07 18:19:22 -0400

    more with methods and pointer receivers

--- methods-with-pointer-receivers.go
+++ methods-with-pointer-receivers.go
@@ -1,4 +1,34 @@
+/*
+https://tour.golang.org/methods/8
+
+!!!
+In general, all methods on a type should have either value or pointer receivers, but not a mixture of both.
+!!!
+ */
+
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+type vertex struct {
+	x, y float64
+}
+
+func (v *vertex) scale(f float64) {
+	v.x = v.x * f
+	v.y = v.y * f
+}
+
+func (v *vertex) abs() float64 {
+	return math.Sqrt(v.x*v.x + v.y*v.y)
+}
+
 func main() {
+	v := &vertex{5, 16}
+	fmt.Printf("Before scaling: %+v, abs: %v\n", v, v.abs())
+	v.scale(6)
+	fmt.Printf("After scaling: %+v, abs: %v\n", v, v.abs())
 }
