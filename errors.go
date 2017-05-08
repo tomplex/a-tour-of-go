efdfaece9d17d310ae8af1430014d4a54839214b
tom caruso <carusot42@gmail.com>
2017-05-07 22:11:40 -0400

    errors

--- errors.go
+++ errors.go
@@ -1,1 +1,33 @@
+/*
+https://tour.golang.org/methods/19
+ */
+
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type anError struct {
+	when time.Time
+	what string
+}
+
+func (e  *anError) Error() string {
+	return fmt.Sprintf("at %v, %s", e.when, e.what)
+}
+
+func run() error {
+	return &anError{
+		time.Now(),
+		"It didn't work",
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
+
--- exercise-errors.go
+++ exercise-errors.go
@@ -1,1 +1,39 @@
+/*
+https://tour.golang.org/methods/20
+ */
+
 package main
+
+import (
+	"fmt"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("Cannot Sqrt a negative number: %f", float64(e))
+}
+
+const Zi = 100.0
+
+func Sqrt(x float64) (z float64, err error) {
+
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z = Zi
+
+	step := func() float64 {
+		return z - (z*z - x) / (2 * z)
+	}
+
+	for i := 0; i <= 10; i++ {
+		z = step()
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
